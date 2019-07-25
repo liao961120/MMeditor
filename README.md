82016346fdec5da9f009769c0ab250045baa6cea
liao <liao961120@gmail.com>
2019-07-25 22:14:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Math/Markdown Editor
+
+Automaticly saves markdown editing history (with [sessionStorage](https://developer.mozilla.org/zh-TW/docs/Web/API/Window/sessionStorage)).
+
+Based on [mathquill](https://github.com/mathquill/mathquill) and [markdown-js](https://github.com/evilstreak/markdown-js).
+
+Perfectly usable locally, just [download](https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/liao961120/MMeditor) this repo.
---
+++ custom.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    sizeIframe();
+    window.onresize = sizeIframe; 
+
+
+    // local storage
+    if (!sessionStorage.getItem('markdown-local'))
+        sessionStorage.setItem('markdown-local', '')
+    else 
+        document.querySelector('textarea#text-input').value = sessionStorage.getItem('markdown-local');
+    
+    // update storage
+    document.querySelector('textarea#text-input').addEventListener('input', function() {
+        let content = this.value;
+        sessionStorage.setItem('markdown-local', content)
+    });
+});
+
+
+function sizeIframe() {
+    var iframe = document.querySelector('iframe');
+    iframe.height = window.innerHeight * 0.95;
+}
