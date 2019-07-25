85036f2635aee015a466190b14862b8b64f9bb45
liao <liao961120@gmail.com>
2019-07-25 23:52:58 +0800

    update

--- custom.js
+++ custom.js
@@ -22,9 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
             {
                 delimiters: [
                     {left: "$$", right: "$$", display: true},
-                    {left: "\\[", right: "\\]", display: true},
                     {left: "$", right: "$", display: false},
-                    {left: "\\(", right: "\\)", display: false}
                 ]
         });
     
